d1d9804f7619d33c346229e0adf78b600b25fd0e
Trevin Small <trevincub03@gmail.com>
2022-08-06 00:41:13 -0400

    added screen sizing and keyboard sizing

--- dist/snake.js
+++ dist/snake.js
@@ -1,8 +1,31 @@
+const pxToREM = 16;
+
 const Snake = ( () => {
 
   let snake = [];
   let board = [];
 
-  
+  const gameContainer = document.getElementById('game-container');
+  const gameWindow = document.getElementById('game-window');
+  const keyboardContainer = document.getElementById('keyboard-container');
+  const gameContainerBorders = 2 * (2 * pxToREM);
+
+  function resize() {
+    keyboardContainer.style.width = keyboardContainer.offsetHeight + "px";
+
+    let min = gameContainer.offsetHeight < gameContainer.offsetWidth ? gameContainer.offsetHeight : gameContainer.offsetWidth;
+
+    let h = gameWindow.style.height = gameWindow.style.width = (min - gameContainerBorders) + "px";
+
+    console.log(h);
+
+    setTimeout(resize, 500);
+  }
+
+  return {
+    resize
+  }
 
 })();
+
+Snake.resize();
