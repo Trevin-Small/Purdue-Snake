ab6c2c359f3392dbdea07eb00a1599458192ddd8
Trevin Small <trevincub03@gmail.com>
2022-08-06 17:41:09 -0400

    Responsive layout & game rendering
    Improved responsive layout: better screen resizing to fit device
    
    Wrote game rendering engine: resizes canvas and draws snake to screen

--- dist/snake.js
+++ dist/snake.js
@@ -2,30 +2,74 @@ const pxToREM = 16;
 
 const Snake = ( () => {
 
-  let snake = [];
-  let board = [];
+  let snake = [[0,0], [1,0], [2,0], [2,1]];
+  let apple = [];
+  let gridBoxSize = 0;
+  let gridBoxMargins = 3;
+  let gridDimension = 16;
+  let windowBorder = 2;
+  let canvasBorder = 4;
 
   const gameContainer = document.getElementById('game-container');
   const gameWindow = document.getElementById('game-window');
+  const gameCanvas = document.getElementById('game-canvas');
+  const ctx = gameCanvas.getContext('2d');
   const keyboardContainer = document.getElementById('keyboard-container');
   const gameContainerBorders = 2 * (2 * pxToREM);
+  const FPS = 20;
 
   function resize() {
     keyboardContainer.style.width = keyboardContainer.offsetHeight + "px";
 
-    let min = gameContainer.offsetHeight < gameContainer.offsetWidth ? gameContainer.offsetHeight : gameContainer.offsetWidth;
+    let minDimension = gameContainer.offsetHeight < gameContainer.offsetWidth ? gameContainer.offsetHeight : gameContainer.offsetWidth;
 
-    let h = gameWindow.style.height = gameWindow.style.width = (min - gameContainerBorders) + "px";
+    let maxDimension = (minDimension - gameContainerBorders);
 
-    console.log(h);
+    let windowDimension = (Math.floor(maxDimension / gridDimension) * gridDimension) + (windowBorder * 2);
 
-    setTimeout(resize, 500);
+    gameWindow.style.height = gameWindow.style.width = windowDimension +  "px";
+
+    gameContainer.style.height = (windowDimension + gameContainerBorders) + "px";
+
+    gameCanvas.width = gameCanvas.height = (windowDimension - canvasBorder * 2);
+
+    console.log(windowDimension);
+
+    gridBoxSize = gameCanvas.width / gridDimension;
+
+    window.onresize = resize;
+  }
+
+  function drawGame() {
+
+    /*
+    ctx.fillStyle = 'none';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    */
+
+    for (let i = 0; i < snake.length; i++) {
+      // outlined square X: 50, Y: 35, width/height 50
+      ctx.beginPath();
+      ctx.fillStyle = 'white';
+      ctx.rect(snake[i][0] * gridBoxSize + gridBoxMargins, snake[i][1] * gridBoxSize + gridBoxMargins, gridBoxSize - gridBoxMargins, gridBoxSize - gridBoxMargins);
+      ctx.fill();
+      ctx.stroke();
+    }
+
+    setTimeout(drawGame, 1000 / FPS);
+
+  }
+
+  function gameStart() {
+    drawGame();
   }
 
   return {
-    resize
+    resize,
+    gameStart
   }
 
 })();
 
 Snake.resize();
+Snake.gameStart();
